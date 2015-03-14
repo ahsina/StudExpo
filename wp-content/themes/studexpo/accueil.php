8acc1e2dd03ba6dbc0081d157df81d98b456183f
abdelazizsakhi <abdelaziz.sakh@gmail.com>
2015-03-14 13:29:45 +0100

    14/03/15 Az

---
+++ wp-content/themes/studexpo/accueil.php
@@ -0,0 +1,204 @@
+<?php
+/*
+Template Name: contact
+*/
+get_header(); ?>
+
+	<div class="page page-index">
+		<!-- SLIDER -->
+		<div class="swiper-container slider page-index__slider">
+			<a href="" class="btn btn-default slider__next"><i class="fa fa-forward"></i></a>
+			<a href="" class="btn btn-default slider__prev"><i class="fa fa-backward"></i></a>
+			<div class="swiper-wrapper">
+				<div style="background-image: url(img/temp/city-wallpaper-3.png)" data-title="Explorer New Waters" data-description="It's not a shower. It's an experience." class="swiper-slide">
+					<a href="" class="swiper-slide__link">
+						<div class="swiper-slide__title">
+							<h4>Vestibulum<small>Lorem ipsum dolor sit amet gros.</small></h4>
+						</div>
+					</a>
+				</div>
+				<div style="background-image: url(img/temp/city-wallpaper-3.png)" class="swiper-slide">
+					<a href="" class="swiper-slide__link"></a>
+				</div>
+				<div style="background-image: url(img/temp/city-wallpaper-3.png)" class="swiper-slide">
+					<a href="" class="swiper-slide__link"></a>
+				</div>
+				<div style="background-image: url(img/temp/city-wallpaper-3.png)" data-video="http://www.youtube.com/embed/NrgWDFGQEE0" data-title="Explorer New Waters" data-description="It's not a shower. It's an experience." class="swiper-slide">
+					<a href="modal-video.php" class="btn btn-default swiper-slide__video fancybox.ajax"></a>
+					<a href="" class="swiper-slide__link">
+						<div class="swiper-slide__title">
+							<h4>Vestibulum<small>Lorem ipsum dolor sit amet gros.</small></h4>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="swiper-pagination"></div>
+		<!-- END SLIDER -->
+		<div class="introduction">
+			<div class="container">
+				<div class="bloc-notif">
+					<button class="view-more">Plus +</button>
+					<h2>Titre du bloc</h2>
+					<p>
+						Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur. quae singula narrare non refert, me professione modum, quod evitandum est, excedamus.
+						Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.
+					</p>
+				</div>
+			</div>
+			<div class="container">
+				<div class="container-bloc row">
+					<div class="index-bloc span">
+						<a href="#">
+							<div class="icon">
+								<i class="fa fa-university"></i>
+							</div>
+							<h3>EXPOSER</h3>
+							<p>Lorem ipsum dolor sit amet. Equitis Romani autem esse filium criminis loco poni ab accusatoribus neque his iudicantibus oportuit.</p>
+							<button class="read-more">En savoir +</button>
+						</a>
+					</div>
+					<div class="index-bloc span2">
+						<a href="#">
+							<div class="icon">
+								<i class="fa fa-graduation-cap"></i>
+							</div>
+							<h3>VISITER</h3>
+							<p>Lorem ipsum dolor sit amet. Equitis Romani autem esse filium criminis loco poni ab accusatoribus neque his iudicantibus oportuit.</p>
+							<button class="read-more">En savoir +</button>
+						</a>
+					</div>
+					<div class="index-bloc span2">
+						<a href="#">
+							<div class="icon">
+								<i class="fa fa-trophy"></i>
+							</div>
+							<h3>CONCOURS</h3>
+							<p>Lorem ipsum dolor sit amet. Equitis Romani autem esse filium criminis loco poni ab accusatoribus neque his iudicantibus oportuit.</p>
+							<button class="read-more">En savoir +</button>
+						</a>
+					</div>
+					<div class="index-bloc span2">
+						<a href="#">
+							<div class="icon">
+								<i class="fa fa-briefcase"></i>
+							</div>
+							<h3>PARTENARIAT</h3>
+							<p>Lorem ipsum dolor sit amet. Equitis Romani autem esse filium criminis loco poni ab accusatoribus neque his iudicantibus oportuit.</p>
+							<button class="read-more">En savoir +</button>
+						</a>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<div class="content">
+			<div class="container-text">
+				<div class="container">
+					<h2>Qui sommes nous ?</h2>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div class="container-slider">
+					<div class="slider-diapo">
+						<div id="owl-demo">
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						  <div class="item"><center><h1>LOGO</h1></center></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="last-container">
+			<div class="last-container-content">
+				<div class="left">
+					<div class="row-intro">
+						<h3>Pourquoi nous choisir</h3>
+						<ul class="why-us">
+							<li>
+								<a href="">
+									Lorem ipsum dolor
+								</a>
+							</li>
+							<li>
+								<a href="">
+									Lorem ipsum dolor
+								</a>
+							</li>
+							<li>
+								<a href="">
+									Lorem ipsum dolor
+								</a>
+							</li>
+							<li>
+								<a href="">
+									Lorem ipsum dolor
+								</a>
+							</li>
+							<li>
+								<a href="">
+									Lorem ipsum dolor
+								</a>
+							</li>
+							<li>
+								<a href="">
+									Lorem ipsum dolor
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="center">
+					<div class="center-text">
+						<p>
+							Post hoc impie perpetratum quod in aliis quoque iam timebatur, 
+							tamquam licentia crudelitati indulta per suspicionum nebulas 
+							aestimati quidam noxii damnabantur. 
+							quorum pars necati, alii puniti bonorum.
+							Post hoc impie perpetratum quod in aliis quoque iam timebatur, 
+							tamquam licentia crudelitati indulta per suspicionum nebulas 
+							aestimati quidam noxii damnabantur. 
+							quorum pars necati, alii puniti bonorum.
+							Post hoc impie perpetratum quod in aliis quoque iam timebatur, 
+							tamquam licentia crudelitati indulta per suspicionum nebulas 
+							aestimati quidam noxii damnabantur. 
+							quorum pars necati, alii puniti bonorum.
+						</p>
+						<div class="container-skills">
+							<div class="bloc-skills">
+								<span class="num">234</span>
+								<span class="attr">Visiteurs</span>
+							</div>
+							<div class="bloc-skills">
+								<span class="num">124</span>
+								<span class="attr">Participants</span>
+							</div>
+							<div class="bloc-skills">
+								<span class="num">49</span>
+								<span class="attr">Stands</span>
+							</div>
+							<div class="bloc-skills">
+								<span class="num">100</span>
+								<span class="attr">Associations</span>
+							</div>
+						</div>
+
+					</div>
+					<div class="counter">
+						<div class="row-intro">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+		<!-- GALLERY -->
+	</div>
+
+
+<?php get_footer(); ?>
